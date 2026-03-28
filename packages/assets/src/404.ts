15a052fb2668851a9766ce307c8eb31d59f10614
Viggo Gascou <contact@viggogascou.co>
2026-03-28 15:48:15 +0100

    fix: use <link> tag for Google Fonts to support Cloudflare Fonts optimization

--- packages/assets/src/404.ts
+++ packages/assets/src/404.ts
@@ -3,8 +3,8 @@ export const html = `<!doctype html>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>404 - Page Not Found</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono">
 <style>
-    @import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
 
     html,
     body {
